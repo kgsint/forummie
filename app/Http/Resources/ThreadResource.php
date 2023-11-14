6eb951e37f76c3dcd21486a64c0d8c2e35812bf1
Kaung Sint <kaungsintkoko99@gmail.com>
2023-11-14 21:17:41 +0630

    fix: datetime and latest post not displaying in index page of forum

--- app/Http/Resources/ThreadResource.php
+++ app/Http/Resources/ThreadResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ThreadResource extends JsonResource
--- app/Repositories/ThreadRepository.php
+++ app/Repositories/ThreadRepository.php
@@ -18,7 +18,7 @@ class ThreadRepository implements ThreadInterface
     public function getFilterablePaginatedCollection()
     {
         return QueryBuilder::for(Thread::class)
-                                ->with(['topic', 'user', 'posts.thread.latestPost.user']) // eager load
+                                ->with(['topic', 'user', 'posts.thread.user', 'latestPost.user']) // eager load
                                 ->allowedFilters($this->customAllowedFilters()) // custom filters
                                 ->orderByLatestPost()
                                 ->orderBy('created_at', 'desc')
