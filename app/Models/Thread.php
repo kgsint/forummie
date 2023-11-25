3ddfadb807cf24b777513bbdbdfa69309049f18c
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-11-25 14:51:35 +0630

    add pivot table for thread_mention

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Spatie\LaravelMarkdown\MarkdownRenderer;
@@ -73,6 +74,13 @@ class Thread extends Model
     public function solution()
     {
         return $this->belongsTo(Post::class, 'solution_post_id');
+    }
+
+    // a list of user being mentioned in a thread
+    public function mentions(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'thread_mention', 'thread_id')
+                                                                            ->withTimestamps();
     }
     // --
 }
