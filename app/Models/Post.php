790cd6607f0b93b194d9b71777d51cc568bf51be
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-11-25 14:15:50 +0630

    create pivot table for mentions relating users and posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
@@ -23,6 +24,13 @@ class Post extends Model
     public function thread(): BelongsTo
     {
         return $this->belongsTo(Thread::class);
+    }
+
+    // a list of users being mentioned
+    public function mentions(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'mentions', 'post_id')
+                                                                    ->withTimestamps();
     }
 
     // nested relationship
