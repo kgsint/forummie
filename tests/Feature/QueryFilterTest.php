27440b70b68a0918dde498d9948ddac8672d031b
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-11-27 12:53:35 +0630

    test: add test for resolved threads query filter

--- tests/Feature/QueryFilterTest.php
+++ tests/Feature/QueryFilterTest.php
@@ -13,6 +13,7 @@ class QueryFilterTest extends TestCase
 {
     use RefreshDatabase;
 
+    // filter no replies test
     public function test_it_can_filter_thread_with_no_replies()
     {
         // total of 3 threads
@@ -24,6 +25,7 @@ class QueryFilterTest extends TestCase
         Post::factory()->create(['thread_id' => $threadTwo->id]);
         Post::factory()->create(['thread_id' => $threadThree->id]);
 
+        // filter response
         $response = $this->get(route('forum.index', ['filter[noreplies]' => '1']));
         $response->assertSuccessful();
 
@@ -33,6 +35,35 @@ class QueryFilterTest extends TestCase
                                         ->where('threads.data.0.title', $threadOne->title)
                                         ->where('threads.data.0.body', "<p>{$threadOne->body}</p>\n") // markdown to html
                                         ->where('threads.data.0.body_markdown', $threadOne->body) // raw body
+        );
+    }
+
+    // filter resolved thread test
+    public function test_it_can_filter_thread_with_solution()
+    {
+        $threadOne = Thread::factory()->create();
+        $threadTwo = Thread::factory()->create();
+        $threadThree = Thread::factory()->create();
+
+        $solutionPost = Post::factory()->create(['thread_id' => $threadOne->id]);
+        Post::factory()->create(['thread_id' => $threadTwo->id]);
+        Post::factory()->create(['thread_id' => $threadThree->id]);
+
+        // mark as best answer
+        $threadOne->solution_post_id = $solutionPost->id;
+        $threadOne->save();
+
+        // filter response
+        $response = $this->get(route('forum.index', ['filter[resolved]' => '1']));
+        $response->assertSuccessful();
+
+        // end point test
+        $response->assertInertia(
+            fn(Assert $page)
+            => $page->has('threads.data', 1)
+                    ->where('threads.data.0.title', $threadOne->title)
+                    ->where('threads.data.0.body', "<p>{$threadOne->body}</p>\n") // markdown to html
+                    ->where('threads.data.0.body_markdown', $threadOne->body) // raw body
         );
     }
 }
