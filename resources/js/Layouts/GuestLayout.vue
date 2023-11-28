9ad183536eb9ee1ff475c055a26df7fc78f519dc
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-11-28 22:56:46 +0630

    update style in guest layout

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,15 +1,160 @@
 <script setup>
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
+    <nav class="bg-white py-3 border-b border-gray-100 h-[6rem]">
+        <!-- Primary Navigation Menu -->
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <Link href="/">
+                            <ApplicationLogo
+                                class="block h-9 w-auto fill-current text-gray-800"
+                            />
+                        </Link>
+                    </div>
+
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <NavLink href="/" :active="route().current('forum.index')">
+                            Forum
+                        </NavLink>
+                    </div>
+                </div>
+
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <!-- Settings Dropdown -->
+                    <div class="ml-3 relative" v-if="$page.props.auth.user">
+                        <Dropdown align="right" width="48">
+                            <template #trigger>
+                                <span class="inline-flex rounded-md">
+                                    <button
+                                        type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                    >
+                                        <span v-if="$page.props.auth.user">{{ $page.props.auth.user.name }}</span>
+                                        <span v-else>Guest</span>
+
+                                        <svg
+                                            class="ml-2 -mr-0.5 h-4 w-4"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd"
+                                            />
+                                        </svg>
+                                    </button>
+                                </span>
+                            </template>
+
+                            <template #content>
+                                <DropdownLink :href="route('profile.edit')"> Settings </DropdownLink>
+                                <DropdownLink :href="route('logout')" method="post" as="button">
+                                    Log Out
+                                </DropdownLink>
+                            </template>
+                        </Dropdown>
+                    </div>
+                    <!-- if guest -->
+                    <div class="flex items-center justify-between space-x-2" v-else>
+                        <Link :href="route('login')">
+                            <PrimaryButton>Login</PrimaryButton>
+                        </Link>
+                        <Link :href="route('register')">
+                            <SecondaryButton>Register</SecondaryButton>
+                        </Link>
+                    </div>
+                </div>
+
+                <!-- Hamburger -->
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <button
+                        @click="showingNavigationDropdown = !showingNavigationDropdown"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                    >
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path
+                                :class="{
+                                    hidden: showingNavigationDropdown,
+                                    'inline-flex': !showingNavigationDropdown,
+                                }"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            />
+                            <path
+                                :class="{
+                                    hidden: !showingNavigationDropdown,
+                                    'inline-flex': showingNavigationDropdown,
+                                }"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Responsive Navigation Menu -->
+        <div
+            :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
+            class="sm:hidden"
+        >
+            <div class="pt-2 pb-3 space-y-1">
+                <ResponsiveNavLink :href="route('forum.index')" :active="route().current('forum.index')">
+                    Forum
+                </ResponsiveNavLink>
+            </div>
+
+            <!-- Responsive nav items (auth) -->
+            <div class="pt-4 pb-1 border-t border-gray-200" v-if="$page.props.auth.user">
+                <div class="px-4">
+                    <div class="font-medium text-base text-gray-800">
+                        {{ $page.props.auth.user.name }}
+                    </div>
+                    <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
+                </div>
+
+                <div class="mt-3 space-y-1">
+                    <ResponsiveNavLink :href="route('profile.edit')" :active="route().current('profile.edit')"> Settings </ResponsiveNavLink>
+                    <ResponsiveNavLink :href="route('logout')" method="post" as="button">
+                        Log Out
+                    </ResponsiveNavLink>
+                </div>
+            </div>
+            <!-- Responsive nav item (guest) -->
+            <div class="pt-4 pb-1 border-t border-gray-200" v-else>
+                <ResponsiveNavLink :href="route('login')"> Login </ResponsiveNavLink>
+                <ResponsiveNavLink :href="route('register')">
+                    Register
+                </ResponsiveNavLink>
+            </div>
+        </div>
+    </nav>
+    <div class="min-h-screen flex sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100" style="min-height: calc(100vh - 6rem);">
+        <!-- <div>
             <Link href="/">
                 <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
             </Link>
-        </div>
+        </div> -->
 
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
