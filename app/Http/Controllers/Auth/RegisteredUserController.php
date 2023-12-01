86c4c8baa5544049620fa288a079bf9a16c721f4
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-01 12:33:38 +0630

    add type field when registering or creating an account in RegisterUserController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,6 +46,7 @@ class RegisteredUserController extends Controller
             'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'type' => User::DEFAULT,
         ]);
 
         event(new Registered($user));
