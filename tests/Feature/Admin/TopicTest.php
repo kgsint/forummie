c8dd7a96677d11730be370d892504c659f87b1f9
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-11 02:23:21 +0630

    add update topic tests

--- tests/Feature/Admin/TopicTest.php
+++ tests/Feature/Admin/TopicTest.php
@@ -99,18 +99,77 @@ class TopicTest extends TestCase
                                                         ])
                                                     );
 
-        $response->assertInvalid(['name' => 'The name has already been taken.']);
-        $response = $this->actingAs($admin)->post(
-            route(
-                'admin.topics.store',
-                $attributes = [
-                    'name' => 'LaraChad',
-                    'slug' => 'laravel',
+        $response->assertInvalid([
+            'name' => 'The name has already been taken.',
+            'slug' => 'The slug has already been taken.'
+        ]);
+
+    }
+
+    public function test_only_admin_or_moderator_can_update_topic()
+    {
+        $admin = User::factory()->create(['type' => User::ADMIN]);
+        $moderator = User::factory()->create(['type' => User::MODERATOR]);
+        $user = User::factory()->create();
+
+        $topicOne = Topic::create(['name' => 'Topic One', 'slug' => 'topic-one']);
+        $topicTwo = Topic::create(['name' => 'Topic Two', 'slug' => 'topic-two']);
+        $topicThree = Topic::create(['name' => 'Topic Three', 'slug' => 'topic-three']);
+
+        // admin request
+        $this->actingAs($admin)
+                ->patch(route('admin.topics.update', ['topic' => $topicOne]), $topicOneAttributes = [
+                    'name' => 'Update Topic One',
+                    'slug' => 'update-topic-one'
                 ])
-            );
+                ->assertRedirectToRoute('admin.topics.index');
 
-            $response->assertInvalid(['slug' => 'The slug has already been taken.']);
+        $this->assertDatabaseHas('topics', $topicOneAttributes);
 
+        // moderator request
+        $this->actingAs($moderator)
+                ->patch(route('admin.topics.update', ['topic' => $topicTwo]), $topicTwoAttributes = [
+                    'name' => 'Update Topic Two',
+                    'slug' => 'update-topic-two'
+                ])
+                ->assertRedirectToRoute('admin.topics.index');
+
+        $this->assertDatabaseHas('topics', $topicTwoAttributes);
+
+        // user request
+        $this->actingAs($user)
+                ->patch(route('admin.topics.update', ['topic' => $topicThree]), $topicThreeAttributes = [
+                    'name' => 'Update Topic Three',
+                    'slug' => 'update-topic-three'
+                ])
+                ->assertForbidden();
+
+        $this->assertDatabaseMissing('topics', $topicThreeAttributes);
+    }
+
+    // validation test for updating topic
+    public function test_validation_works_correctly_updating_topic()
+    {
+        $admin = User::factory()->create(['type' => User::ADMIN]);
+
+        $topic = Topic::create(['name' => 'Topic One', 'slug' => 'topic-one']);
+        $anotherTopic = Topic::create(['name' => 'Another Topic', 'slug' => 'another-topic']);
+
+        // required
+        $this->actingAs($admin)
+                ->patch(route('admin.topics.update', ['topic' => $topic]), [
+                    'name' => '',
+                    'slug' => ''
+                ])
+                ->assertValid(['name' => 'The name cannot be empty', 'slug' => 'The slug cannot be empty']);
+
+        // unique
+        $this->actingAs($admin)
+        ->patch(route('admin.topics.update', ['topic' => $topic]), [
+            'name' => 'Another Topic',
+            'slug' => 'another-topic'
+        ])
+        ->assertValid(['name' => 'The name has already been taken.', 'slug' => 'The slug has already been taken.']);
     }
 
     // delete
