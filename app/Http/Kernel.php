c81defddbd4eb8157b4d01290595ce764b8dd4c5
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-11 17:53:20 +0630

    register RedirectIfBanned middleware in Kernel's web routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\RedirectIfBanned;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -38,6 +39,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            RedirectIfBanned::class,
         ],
 
         'api' => [
