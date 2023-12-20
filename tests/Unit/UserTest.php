0b1e8f8a8ddcafae524905d95376b66cb9ee2c6e
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-20 23:17:50 +0630

    test: add more unit tests for User model

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Models\Post;
+use App\Models\Thread;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -26,5 +29,63 @@ class UserTest extends TestCase
         ]);
 
         $this->assertTrue($user->isModerator());
+    }
+
+    public function test_it_default_user()
+    {
+        $user = User::factory()->create();
+
+        $this->assertSame(User::DEFAULT, $user->type);
+    }
+
+    public function test_user_has_threads()
+    {
+        $user = User::factory()->create();
+        Thread::factory(3)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertInstanceOf(Collection::class, $user->threads);
+    }
+
+    public function test_user_has_posts_or_replies()
+    {
+        $user = User::factory()->create();
+        Post::factory(2)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertInstanceOf(Collection::class, $user->posts);
+    }
+
+    public function test_user_can_be_banned()
+    {
+        $user = User::factory()->create();
+
+        $this->assertFalse($user->isBanned());
+
+        $user->ban('spamming');
+
+        $this->assertTrue($user->isBanned());
+    }
+
+    public function test_user_can_be_unban()
+    {
+        $user = User::factory()->create();
+
+        $user->ban('spamming');
+        $this->assertTrue($user->isBanned());
+
+        $user->unban();
+        $this->assertFalse($user->isBanned());
+    }
+
+    public function test_user_have_default_avatar()
+    {
+        $user = User::factory()->create();
+
+        $this->assertTrue(
+            str_contains($user->getAvatar(), 'https://gravatar.com/avatar')
+        );
     }
 }
